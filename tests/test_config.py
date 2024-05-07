16ebc3463508800289dc85bdd1662c86792486ac
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2024-05-07 10:17:59 -0600

    test: add cr

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,24 +2,27 @@
 # vi: set shiftwidth=4 tabstop=8 expandtab:
 # :indentSize=4:tabSize=8:noTabs=true:
 #
+# SPDX-FileCopyrightText: Copyright 2024-present Intel Corporation.
+#
 # SPDX-License-Identifier: GPL-3.0-or-later
 """
 Prueba de config.py
 """
 
-from siesel.config import read_conf_env
 import os
+
 import pytest
 
-@pytest.mark.parametrize("kernel_path,config_file",
-                         [("a_path", "a_file"),
-                          ("a_path", None),
-                          (None, "a_file"),
-                          (None, None)])
+from siesel.config import read_conf_env
+
+
+@pytest.mark.parametrize(
+    "kernel_path,config_file",
+    [("a_path", "a_file"), ("a_path", None), (None, "a_file"), (None, None)],
+)
 def test_read_conf_env(kernel_path, config_file):
-    """ Test the read conf env function """
+    """Test the read conf env function"""
     os.environ["SIESEL_KERNEL_PATH"] = kernel_path
     os.environ["SIESEL_CONFIG_FILE"] = config_file
-    expected = {"kernel_path": kernel_path,
-                "config_file": config_file}
+    expected = {"kernel_path": kernel_path, "config_file": config_file}
     assert expected == read_conf_env(expected)
