2032a6dbd70907ea3db4773d39ae9cdefd9e99a7
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2024-05-13 11:43:44 -0600

    kernel: rename function to is valid git repo

--- src/siesel/kernel.py
+++ src/siesel/kernel.py
@@ -13,30 +13,30 @@ import os
 from sys import stderr
 
 
-def validate_repo(path: str) -> int:
+def is_valid_git_repo(path: str) -> bool:
     """Validate is the kernel git repo"""
 
     if not os.path.exists(path):
         print(f"{path}: not found", file=stderr)
-        return 1
+        return False
 
     p = os.path.join(path, ".git")
     if not os.path.exists(p):
         print(f"{p}: not found. Is not a git repo", file=stderr)
-        return 1
+        return False
 
     p = os.path.join(path, "drivers", "cxl")
     if not os.path.exists(p):
         print(f"{p}: CXL source code not found", file=stderr)
-        return 1
+        return False
 
-    return 0
+    return True
 
 
 def get_cxl_features(kernel_path: str) -> int:
     """Get the CXL features from the kernel repo"""
 
-    if validate_repo(kernel_path):
+    if not is_valid_git_repo(kernel_path):
         return 1
 
     return 0
